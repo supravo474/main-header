fbd4e8c62cc1cbcded9c53317c5a6e7d6698df86
Suprayvo <68844142+supravo474@users.noreply.github.com>
2022-01-08 14:53:32 +0530

    2.0

--- main.cpp
+++ main.cpp
@@ -6,14 +6,22 @@
 using namespace std;
 int main(){
 
-    int number,number2;
+    float number,number2,number3,number4;
+
 
     cout<<"Enter the 1st Number"<<endl;
     cin>>number;
     cout<<"Enter the 2nd Number"<<endl;
     cin>>number2;
+    cout<<"Enter the 3rd Number"<<endl;
+    cin>>number3;
+    /*cout<<"Enter the 4rd Number"<<endl;
+    cin>>number4;*/
 
-    
+
+    //PI Value
+   ///cout<<PI<<endl;
+
     //palindrome
     //checkPalindrome(number);
 
@@ -32,7 +40,7 @@ int main(){
 
     //reverse Number
    // cout<<"\nThe Reverse is: "<<rev(number)<<endl;
-    //number of digits in an integer 
+    //number of digits in an integer
     //cout<<"\nNumber of digits are: "<<countDigit(number)<<endl;
 
     //Armstrong Number
@@ -61,12 +69,12 @@ int main(){
     //neon(number);
    // neon(number2);
 
-   //Factors of a number 
+   //Factors of a number
    //facto(number);
    //cout<<" "<<endl;
    //facto(number2);
 
-  //GDC of tow numbers
+  //GDC of two numbers
    //gdc(number,number2);
 
 
@@ -76,7 +84,7 @@ int main(){
   //power of a number(number1 is base and number 2 is exponent)
   //cout<<powerfo(number,number2)<<endl;
 
-  //Square Root 
+  //Square Root
   //squaro(number);
   //squaro(number2);
 
@@ -85,14 +93,14 @@ int main(){
   //cout<<cubor(number)<<endl;
   //cout<<"Cube root of "<<number2<<" is "<<endl;
   //cout<<cubor(number2)<<endl;
-  
+
   //Prints sum of odd numbers in a given range
   //sumodd(number,number2);
- 
+
 
   //Prints sum of sqares of natural numbers 1 to N
   //sumsqar(number,number2);
- 
+
 
  //Prints sum of all even numbers in a given range
 // sumeven(number,number2);
@@ -113,11 +121,11 @@ int main(){
 
 //Palindrome Numbers in  a given range
 //palermo(number,number2);
-    
+
 //IS power of a number(first place is for the number whom you want to check of and second is the number whom you are checking eg. Powif(2,64))
 //Powif(number,number2);
- 
- //multiply
+
+//multiply
 //mulp(number,number2);
 
 //divide
@@ -128,6 +136,107 @@ int main(){
 
 // substract
 //subn(number,number2);
+
+//area of triangle(takes base and height{can be used for any triangle})
+//areaTri(number,number2);
+
+//area of Circle(takes radius)
+//areaCirc(number);
+
+//area of trapezium takes two bases first then height eg areaTrap(5,12,4)
+//areaTrap(number,number2,number3);
+
+//Rhombus area(takes p{diagonal[1]} at first then q{diagonal[1]})
+//areaRom(number,number2);
+
+// Area of Paralleogram (takes base first then height {eg.areaPar(15,8)} )
+//areaPar(number,number2);
+
+//Cube area
+//areaCube(number);
+
+
+//Cylinder  surface area
+//areaCy(number,number2);
+
+
+//Cube Volume
+//volCube(number);
+
+//Cylinder Volume(takes radius first then height)
+//volCy(number,number2);
+
+
+//Surface area of cuboid(takes width then lenght then height)
+//cubeoidArea(number,number2,number3);
+
+//Volume of cuboid(takes width then lenght then height)
+//cubeoidVol(number,number2,number3);
+
+//Volume of cone(takes radius then height)
+//coneVol(number,number2);
+
+
+//Surface area of cone(takes radius then slant height)
+//coneArea(number,number2);
+
+
+//Sphere Surface area
+//sphArea(number);
+
+//Sphere Volume
+//sphVol(number);
+
+//Perimeter of rectangle takes breath first then lenght
+//perRec(number,number2);
+
+//Perimeter of square
+//perSq(number);
+
+//Perimeter Triangle(takes all three sides)
+//perTri(number,number2,number3);
+
+//Perimeter of Circle takes radius
+//perCir(number);
+
+//Perimeter of Trapezium takes all four sides
+//perTrap(number,number2,number3,number4);
+
+
+//Paralleogram perimeter takes base first then height
+//perPar(number,number2);
+
+
+//Rhombus Perimeter
+//perRom(number);
+
+//Kite perimeter takes both the sides
+//perKit(number,number2);
+
+//Kite Area takes p first then q
+//areaKit(number,number2);
+
+//Polygon Perimeter takes number of sides first then takes lenght of the sides
+//perPoly(number,number2);
+
+//Annulus Perimeter takes R first then r(Note:R>r)
+//perAn(number,number2);
+
+
+//Annulas area takes R first then r(Note:R>r)
+//areaAn(number,number2);
+
+
+//Area of Rectangle takes lenght first then width
+//areaRec(number,number2);
+
+//Compound Interest takes principle first then rate then time Note: due to some technical issues{less knowledge} the before the rate there should be 0. ex 5.4 = 0.054,10 = 0.10,99 = 0.99
+//compoundInt(number,number2,number3);
+
+//Simple Interest takes principle first then rate then time
+//simpleInt(number,number2,number3);
+
+
    return 0;
 
 }
